fc61727a5e1f8567fd01d2ff03b4b435ca90a779
RafaelMachado914 <lafaellascado2004@gmail.com>
2025-08-27 07:48:49 -0300

    Criação do favicon e cursor

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
 function sortearImagem() {
 
-let numeroSorteado = Math.floor(Math.random() * 20) + 1;
+    let numeroSorteado = Math.floor(Math.random() * 20) + 1;
 
-let img = document.getElementById("Rolagem");
+    let img = document.getElementById("Rolagem");
 
-if(numeroSorteado===1){
-    img.src = "imagens/1.png"
-}
-if(numeroSorteado===2){
-    img.src = "imagens/2.png"
-}
-if(numeroSorteado===3){
-    img.src = "imagens/3.png"
-}
-if(numeroSorteado===4){
-    img.src = "imagens/4.png"
-}
-if(numeroSorteado===5){
-    img.src = "imagens/5.png"
-}
-if(numeroSorteado===6){
-    img.src = "imagens/6.png"
-}
-if(numeroSorteado===7){
-    img.src = "imagens/7.png"
-}
-if(numeroSorteado===8){
-    img.src = "imagens/8.png"
-}
-if(numeroSorteado===9){
-    img.src = "imagens/9.png"
-}
-if(numeroSorteado===10){
-    img.src = "imagens/10.png"
-}
-if(numeroSorteado===11){
-    img.src = "imagens/11.png"
-}
-if(numeroSorteado===12){
-    img.src = "imagens/12.png"
-}
-if(numeroSorteado===13){
-    img.src = "imagens/13.png"
-}
-if(numeroSorteado===14){
-    img.src = "imagens/14.png"
-}
-if(numeroSorteado===15){
-    img.src = "imagens/15.png"
-}
-if(numeroSorteado===16){
-    img.src = "imagens/16.png"
-}
-if(numeroSorteado===17){
-    img.src = "imagens/17.png"
-}
-if(numeroSorteado===18){
-    img.src = "imagens/18.png"
-}
-if(numeroSorteado===19){
-    img.src = "imagens/19.png"
-}
-if(numeroSorteado===20){
-    img.src = "imagens/20.png"
-}
+    if (numeroSorteado === 1) {
+        img.src = "imagens/1.png"
+    }
+    if (numeroSorteado === 2) {
+        img.src = "imagens/2.png"
+    }
+    if (numeroSorteado === 3) {
+        img.src = "imagens/3.png"
+    }
+    if (numeroSorteado === 4) {
+        img.src = "imagens/4.png"
+    }
+    if (numeroSorteado === 5) {
+        img.src = "imagens/5.png"
+    }
+    if (numeroSorteado === 6) {
+        img.src = "imagens/6.png"
+    }
+    if (numeroSorteado === 7) {
+        img.src = "imagens/7.png"
+    }
+    if (numeroSorteado === 8) {
+        img.src = "imagens/8.png"
+    }
+    if (numeroSorteado === 9) {
+        img.src = "imagens/9.png"
+    }
+    if (numeroSorteado === 10) {
+        img.src = "imagens/10.png"
+    }
+    if (numeroSorteado === 11) {
+        img.src = "imagens/11.png"
+    }
+    if (numeroSorteado === 12) {
+        img.src = "imagens/12.png"
+    }
+    if (numeroSorteado === 13) {
+        img.src = "imagens/13.png"
+    }
+    if (numeroSorteado === 14) {
+        img.src = "imagens/14.png"
+    }
+    if (numeroSorteado === 15) {
+        img.src = "imagens/15.png"
+    }
+    if (numeroSorteado === 16) {
+        img.src = "imagens/16.png"
+    }
+    if (numeroSorteado === 17) {
+        img.src = "imagens/17.png"
+    }
+    if (numeroSorteado === 18) {
+        img.src = "imagens/18.png"
+    }
+    if (numeroSorteado === 19) {
+        img.src = "imagens/19.png"
+    }
+    if (numeroSorteado === 20) {
+        img.src = "imagens/20.png"
+    }
 }
